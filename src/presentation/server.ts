bbb583a6778da929cd493d575c0520f5c5c76376
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-26 11:24:38 +0100

    Update server console log message

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -31,7 +31,7 @@ export class Server {
     })
 
     this.app.listen(this.port, () =>
-      console.log(`Server running at http://localhost:${this.port}`)
+      console.log(`Server running at port ${this.port}`)
     )
   }
 }
